ab25d8ca254789cab887c5376e1fb075524067cc
Sriman Padmanaban <spadmanaban@cloudbees.com>
2025-03-04 12:32:41 +0530

    Removed printing of env

--- main.go
+++ main.go
@@ -1,20 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"gha-register-build-artifact/cmd"
 	"log"
-	"os"
-	"strings"
 )
 
 func main() {
 
-	for _, env := range os.Environ() {
-		// Split into key and value
-		pair := strings.SplitN(env, "=", 2)
-		fmt.Printf("🔹 %s = %s\n", pair[0], pair[1])
-	}
 	if err := cmd.Execute(); err != nil {
 		log.Fatal(err)
 	}
