895c62ce03f46418e32b0a42402e4aee04fe405b
Sriman Padmanaban <spadmanaban@cloudbees.com>
2025-03-04 10:05:47 +0530

    Reading workflow name

--- main.go
+++ main.go
@@ -1,12 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"gha-register-build-artifact/cmd"
 	"log"
+	"os"
+	"strings"
 )
 
 func main() {
 
+	for _, env := range os.Environ() {
+		// Split into key and value
+		pair := strings.SplitN(env, "=", 2)
+		fmt.Printf("🔹 %s = %s\n", pair[0], pair[1])
+	}
 	if err := cmd.Execute(); err != nil {
 		log.Fatal(err)
 	}
